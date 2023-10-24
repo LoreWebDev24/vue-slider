be699f1746c6c6b1664759befdcf4e9e3fc6a37a
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-24 14:37:32 +0200

    started refactorying the code

---
+++ js/master.js
@@ -0,0 +1,61 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            currentIndex: 0,
+            images : [
+                {
+                    image: 'img/01.webp',
+                    title: "Marvel's Spiderman Miles Morales",
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    length: 'Up to the player',
+                },
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    length: 'Up to the player',
+                },
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+                    length: 'Up to the player',
+                },
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    length: 'Up to the player',
+                },
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+                    length: 'Up to the player',
+                },
+            ]
+        }
+    },
+    methods: {
+        nextImage() {
+            this.currentIndex++;
+            if (this.currentIndex > this.images.length - 1) {
+                this.currentIndex = 0;
+            }
+        },
+        prevImage() {
+            this.currentIndex--;
+            if (this.currentIndex < 0) {
+                this.currentIndex = this.images.length - 1;
+            }
+        },
+        changeImage(thumbIndex) {
+            this.currentIndex = thumbIndex;
+        }
+    },
+    mounted() {
+        console.log("VUE WORKA");
+    }
+}).mount('#app');
